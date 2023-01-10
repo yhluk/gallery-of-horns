872b8551d7b82742c6d4e832f3c15760e619804b
yurii hlukhyi <yhlukhyi@gmail.com>
2023-01-09 19:38:29 -0800

    fixed images

--- src/Main.js
+++ src/Main.js
@@ -5,8 +5,15 @@ class Main extends React.Component{
   render(){
     return(
       <>
-      <HornedBeast title="title" imgUrl="url" description="description"/>
-      <HornedBeast title="title" imgUrl="url" description="description"/>
+      <HornedBeast 
+      title={"1st-beast"} 
+      imgUrl={"https://images.unsplash.com/photo-1502641923341-7d3e5c05bd73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"} description={"small beast"}
+      />
+
+      <HornedBeast 
+      title={"2nd-beast"} 
+      imgUrl={"https://images.unsplash.com/photo-1507824154544-21a54a06a929?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"} description={"big beast"}
+      />
       </>
     )
   }
