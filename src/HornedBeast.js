d9e155192ad32ffe3df2c658efecdd0624f17565
yurii hlukhyi <yhlukhyi@gmail.com>
2023-01-12 22:06:12 -0800

    got saite to live

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,15 +1,42 @@
 import React from "react";
-import data from "./data.json"
+// import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './index.css'
 
 
 class HornedBeast extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      clickedFavorite: 0
+    }
+  }
+
+  handleFavorite = () =>
+  {
+    this.setState({
+      clickedFavorite: this.state.clickedFavorite + 1
+    });
+  }
+
   render() {
+
     return (
-      <>
-      <h2>{this.props.title}</h2>
-      <img src={this.props.imgUrl} alt={this.props.alt} title={this.props.title}/>
-      <p>{this.props.description}</p>
-      </>
+      <div className = "beast">
+        <Card id="card" style={{ width: '18rem',  height: '30rem'}}>
+          <Card.Img 
+          style={{ height: '18rem' }}
+          src={this.props.image_url}
+          alt={this.props.title}
+          onClick = {this.handleFavorite}
+          />
+          <Card.Title as='h2'>{this.props.title}</Card.Title>
+          <Card.Text>{this.props.description}</Card.Text>
+          <Card.Text> I like it! {this.state.clickedFavorite} 🫶</Card.Text>
+        </Card>
+
+
+      </div>
     )
   }
 }
--- src/Main.js
+++ src/Main.js
@@ -2,40 +2,20 @@ import React from "react";
 import HornedBeast from "./HornedBeast";
 import data from "./data.json"
 
+
 class Main extends React.Component{
   render(){
     return(
       <>
-        <HornedBeast
-          title={"1st-beast"}
-          imgUrl={"https://images.unsplash.com/photo-1502641923341-7d3e5c05bd73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"} 
-          description={"small beast"}
-        />
-
-        <HornedBeast
-          title={"2nd-beast"}
-          imgUrl={"https://images.unsplash.com/photo-1507824154544-21a54a06a929?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"} 
-          description={"big beast"}
-        />
-
-        <HornedBeast
-          title={"3rd-beast"}
-          imgUrl={"https://images.unsplash.com/photo-1532007195987-bb4ddeaf052d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80"} 
-          description={"weird beast"}
-        />
-
-        <HornedBeast
-          title={"4th-beast"}
-          imgUrl={"https://images.unsplash.com/photo-1592393830922-2e8568bd3d09?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80"} 
-          description={"say what beast"}
-        />
-
-        <HornedBeast
-          title={"5th-beast"}
-          imgUrl={"https://images.unsplash.com/photo-1632665745163-84b8342ee740?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1829&q=80"}
-          description={"outdated beast"}
-        />
-
+      <main>
+        {data.map((hornedBeast) => <HornedBeast 
+          title = {hornedBeast.title}
+          image_url = {hornedBeast.image_url}
+          description = {hornedBeast.description}
+          key = {hornedBeast._id}
+          />
+        )}
+      </main>
       </>
     )
   }
